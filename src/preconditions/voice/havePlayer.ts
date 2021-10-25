702c1cb001ec548762a5f9836dff1f8a85fb49d1
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-10-25 07:14:50 +0700

    feat: add havePlayer conditions

---
+++ src/preconditions/voice/havePlayer.ts
@@ -0,0 +1,28 @@
+import { PreconditionOptions, Precondition } from "@sapphire/framework";
+import { Message } from "discord.js";
+import { ApplyOptions } from "@sapphire/decorators";
+import { isStageChannel, isVoiceChannel } from "@sapphire/discord.js-utilities";
+
+@ApplyOptions<PreconditionOptions>({
+    name: "havePlayer"
+})
+
+
+export abstract class havePlayer extends Precondition {
+    public async run(msg: Message) {
+        const checkIsAllow = this.shouldRun(msg);
+        if (!checkIsAllow) return this.error({ message: "No **player** in this guild" });
+        return this.ok();
+    }
+
+    private shouldRun(msg: Message) {
+        if (!this.container.client.manager.get(msg.guild?.id!)) return false
+        return true;
+    }
+}
+
+declare module "@sapphire/framework" {
+    interface Preconditions {
+        havePlayer: never;
+    }
+}
