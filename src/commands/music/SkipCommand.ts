7d8fe17b8491263aceb1ad7c3c63a0d2b1df7958
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-10-25 19:22:47 +0700

    feat: add skipCommand

---
+++ src/commands/music/SkipCommand.ts
@@ -0,0 +1,26 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Args, Command, CommandOptions } from "@sapphire/framework";
+import { Message, MessageEmbed, MessageActionRow, MessageButton } from "discord.js";
+
+@ApplyOptions<CommandOptions>({
+    name: "skip",
+    aliases: ["s"],
+    requiredClientPermissions: ["CONNECT", "SPEAK"],
+    preconditions: ["inVoice", "sameVoice", "havePlayer", "haveQueue"]
+})
+
+export class MusicCommand extends Command {
+    async messageRun(msg: Message, args: Args) {
+        const player = this.container.client.manager.get(msg.guildId!);
+
+        const autoplay = player?.get("autoplay");
+
+        if (autoplay === false) {
+            player?.stop();
+        } else {
+            player?.stop();
+            player?.set("autoplay", false);
+        }
+        msg.react('⏭️').catch(e => { })
+    }
+}
