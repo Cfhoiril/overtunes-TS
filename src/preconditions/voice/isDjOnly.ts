64dadecfd75d9f2ac281c8ba137065f986b02e65
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-10-26 07:42:52 +0700

    fix(djOnly): remove console test

--- src/preconditions/voice/isDjOnly.ts
+++ src/preconditions/voice/isDjOnly.ts
@@ -12,31 +12,15 @@ export abstract class isDjOnly extends Precondition {
     public async run(msg: Message) {
         const djOnly = await setting.findOne({ Guild: msg.guild?.id! });
         if (djOnly.Dj == true) {
-            console.log("dj true")
             if (msg.member?.voice?.channel && !msg.member?.permissions.has('MANAGE_CHANNELS') && !msg.member?.roles?.cache.some(role => role.name === 'DJ') && (msg.member?.voice?.channel?.members.filter(m => !m.user.bot)?.size! > 1)) {
-                console.log("ga sendiri")
                 return this.error({ message: "You need Role named **DJ** or **MANAGE_CHANNELS** permissions to use this commands, Being **ALONE** with the bot also works" })
             } else {
-                console.log("ok")
                 return this.ok();
             }
         } else {
-            console.log("ok")
             return this.ok();
         }
     }
-
-    // private async shouldRun(msg: Message) {
-    //     const djOnly = await setting.findOne({ Guild: msg.guild?.id! });
-    //     if (!djOnly.Dj || djOnly.Dj !== true) {
-    //     } else if (djOnly == true) {
-    //         if (msg.member?.voice?.channel && !msg.member?.permissions.has('MANAGE_CHANNELS') && !msg.member?.roles?.cache.some(role => role.name === 'DJ') && (msg.member?.voice?.channel.members.filter(m => !m.user.bot)?.size > 1)) {
-    //             console.log("ada")
-    //         }
-    //     }
-    //     return false;
-    // }
-
 }
 
 declare module "@sapphire/framework" {
