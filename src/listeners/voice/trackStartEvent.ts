7f383152d6134fd9faaaecbcf216eab9a5d4bebf
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-10-26 19:47:16 +0700

    feat(trackStart): add special channel handler

--- src/listeners/voice/trackStartEvent.ts
+++ src/listeners/voice/trackStartEvent.ts
@@ -1,9 +1,10 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Listener, ListenerOptions } from "@sapphire/framework";
 import { Track, Player, Manager } from "erela.js";
-import { Message, MessageEmbed } from "discord.js";
+import { Message, MessageEmbed, TextChannel, Snowflake } from "discord.js";
 import { TextBasedChannelTypes, VoiceBasedChannelTypes } from "@sapphire/discord.js-utilities";
 import { Client } from "discord.js";
+import Set from "../../database/Manager/MusicManager";
 
 @ApplyOptions<ListenerOptions>({
     name: "trackStart",
@@ -19,14 +20,42 @@ export class trackStartEvent extends Listener {
             this.container.client.guilds.cache.get(player.guild)?.me?.voice.setDeaf(true).catch(e => { });
 
         const channel = this.container.client.channels.cache.get(player.textChannel!) as TextBasedChannelTypes;
-        const a = new MessageEmbed()
-            .setTitle('Now playing')
-            .setColor(this.container.client.guilds.cache.get(player.guild)?.me?.displayHexColor!)
-            .setDescription(`${track.title} [${track.requester}]`)
-            .setTimestamp()
+        let special = await Set.findOne({ Guild: player.guild });
 
-        channel?.send({ embeds: [a] }).then(msgs => {
-            player.set("Message", msgs.id)
-        }).catch(e => { })
+        if (special && special.Channel !== null && special.Message !== null) {
+            channel.messages.fetch(special.Message).catch(e => {
+                special.Channel = null
+                special.Message = null
+
+                special.save()
+                return channel.send({
+                    embeds: [new MessageEmbed()
+                        .setDescription('Template messages not found, back to normal mode')
+                        .setColor('RED')
+                    ]
+                })
+            });
+
+            channel.messages.fetch({
+                limit: 10
+            }).then((messages) => {
+                messages.filter(m => m.id != special.Message).map(delMessage => {
+                    delMessage.delete()
+                })
+            })
+        } else {
+            if (special && special.Channel === player.textChannel) return;
+            if (special.Announce === true) {
+                const a = new MessageEmbed()
+                    .setTitle('Now playing')
+                    .setColor(this.container.client.guilds.cache.get(player.guild)?.me?.displayHexColor!)
+                    .setDescription(`${track.title} [${track.requester}]`)
+                    .setTimestamp()
+
+                channel?.send({ embeds: [a] }).then(msgs => {
+                    player.set("Message", msgs.id)
+                }).catch(e => { })
+            }
+        }
     }
 }
