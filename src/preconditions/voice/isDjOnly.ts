c38ee9b611a6c121e04c3940eb85716003160004
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-10-26 07:41:36 +0700

    fix(djOnly): fix djOnly not emitted

--- src/preconditions/voice/isDjOnly.ts
+++ src/preconditions/voice/isDjOnly.ts
@@ -10,20 +10,32 @@ import setting from "../../database/Manager/MusicManager";
 export abstract class isDjOnly extends Precondition {
 
     public async run(msg: Message) {
-        const allow = this.shouldRun(msg);
-        if (!allow) return this.error({ message: "You need Role named **DJ** or **MANAGE_CHANNELS** permissions to use this commands, Being **ALONE** with the bot also works" })
-        return this.ok();
+        const djOnly = await setting.findOne({ Guild: msg.guild?.id! });
+        if (djOnly.Dj == true) {
+            console.log("dj true")
+            if (msg.member?.voice?.channel && !msg.member?.permissions.has('MANAGE_CHANNELS') && !msg.member?.roles?.cache.some(role => role.name === 'DJ') && (msg.member?.voice?.channel?.members.filter(m => !m.user.bot)?.size! > 1)) {
+                console.log("ga sendiri")
+                return this.error({ message: "You need Role named **DJ** or **MANAGE_CHANNELS** permissions to use this commands, Being **ALONE** with the bot also works" })
+            } else {
+                console.log("ok")
+                return this.ok();
+            }
+        } else {
+            console.log("ok")
+            return this.ok();
+        }
     }
 
-    private async shouldRun(msg: Message) {
-        const djOnly = await setting.findOne({ Guild: msg.guild?.id! });
-        if (djOnly === true) {
-            if (msg.member?.voice?.channel && !msg.member?.permissions.has('MANAGE_CHANNELS') && !msg.member.roles.cache.some(role => role.name === 'DJ')) {
-                return false;
-            }
-        }
-        return true;
-    }
+    // private async shouldRun(msg: Message) {
+    //     const djOnly = await setting.findOne({ Guild: msg.guild?.id! });
+    //     if (!djOnly.Dj || djOnly.Dj !== true) {
+    //     } else if (djOnly == true) {
+    //         if (msg.member?.voice?.channel && !msg.member?.permissions.has('MANAGE_CHANNELS') && !msg.member?.roles?.cache.some(role => role.name === 'DJ') && (msg.member?.voice?.channel.members.filter(m => !m.user.bot)?.size > 1)) {
+    //             console.log("ada")
+    //         }
+    //     }
+    //     return false;
+    // }
 
 }
 
