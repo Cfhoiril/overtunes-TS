48630579574bf2671f286e18494b4735fa040b33
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-10-26 13:13:50 +0700

    feat: add track error event

---
+++ src/listeners/voice/trackErrorEvent.ts
@@ -0,0 +1,26 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Listener, ListenerOptions } from "@sapphire/framework";
+import { Track, Player, Manager } from "erela.js";
+import { Message, MessageEmbed } from "discord.js";
+import { TextBasedChannelTypes, VoiceBasedChannelTypes } from "@sapphire/discord.js-utilities";
+import { Client } from "discord.js";
+
+@ApplyOptions<ListenerOptions>({
+    name: "trackError",
+    emitter: "manager" as keyof Client,
+    event: "trackError"
+})
+
+export class trackErrorEvent extends Listener {
+    async run(player: Player, track: Track) {
+        if (!player?.voiceChannel) player.destroy();
+        const channel = this.container.client.channels.cache.get(player.textChannel!) as TextBasedChannelTypes;
+
+        if (player.textChannel) channel.messages.fetch(player.get("Message")).then(x => x.delete()).catch(e => { });
+        channel.send({
+            embeds: [new MessageEmbed()
+                .setColor("RED")
+                .setDescription("Track is error, Skipping to next song")]
+        }).then(ms => setTimeout(function () { ms.delete() }, 10000))
+    }
+}
