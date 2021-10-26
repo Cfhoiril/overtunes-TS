6c7416214859378eb7af304717c3430b981716fd
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-10-26 13:08:44 +0700

    feat: add nodeConnect event

---
+++ src/listeners/nodes/nodeConnectEvent.ts
@@ -0,0 +1,18 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Listener, ListenerOptions } from "@sapphire/framework";
+import { Track, Player, Node } from "erela.js";
+import { Client } from "discord.js";
+import stay from "../../database/Manager/MusicManager";
+import chalk from "chalk";
+
+@ApplyOptions<ListenerOptions>({
+    name: "nodeConnect",
+    emitter: "manager" as keyof Client,
+    event: "nodeConnect"
+})
+
+export class playerMoveEvent extends Listener {
+    async run(node: Node) {
+        console.log(chalk.green(`🎧 ${node.options.identifier} Connected`))
+    }
+}
