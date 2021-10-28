ba80ab2182f15ab9e10a4e6fb89067f2a653efb7
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-10-29 00:03:00 +0700

    feat(readyEvent): handle player move to readyEvent

--- src/listeners/client/readyEvent.ts
+++ src/listeners/client/readyEvent.ts
@@ -3,6 +3,7 @@ import { Listener, ListenerOptions } from "@sapphire/framework";
 import chalk from "chalk";
 import * as config from "../../config.json";
 import mongoose, { connect } from "mongoose";
+import { Node } from "erela.js";
 
 @ApplyOptions<ListenerOptions>({
     name: "ready",
@@ -27,6 +28,27 @@ export class readyEvent extends Listener {
 
         this.container.client.manager.init(this.container.client.user!.id);
         this.container.logger.info(chalk.green(`👋 Logged in as ${chalk.white(this.container.client.user?.username)}`));
+
+        // Node disconnect handle to move player
+        this.container.client.manager.on("nodeDisconnect", async (node: Node) => {
+            for (const players of [...this.container.client.manager.players.filter(x => x.node === node).values()]) {
+                const newNode = this.container.client.manager.nodes.get(this.container.client?.manager?.leastLoadNodes.first()?.options.identifier as string);
+
+                const playOptions = {
+                    op: "play",
+                    guildId: players.guild,
+                    track: players.queue.current?.track,
+                    startTime: players.position,
+                    volume: players.volume,
+                };
+
+                await newNode?.send(players.voiceState);
+                await newNode?.send(playOptions);
+                // @ts-ignore
+                players.node = newNode;
+                console.log(chalk.yellow(`🏹 ${players.guild}'s player moved to ${newNode?.options.identifier}`))
+            }
+        })
     }
 }
 
