aec101ae19d5849b61fe52f4004605ad93071bc8
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-10-28 23:02:33 +0700

    fix something

--- src/listeners/nodes/nodeDisconnectEvent.ts
+++ src/listeners/nodes/nodeDisconnectEvent.ts
@@ -3,7 +3,6 @@ import { ApplyOptions } from "@sapphire/decorators";
 import { Listener, ListenerOptions } from "@sapphire/framework";
 import { Track, Player, Node } from "erela.js";
 import { Client } from "discord.js";
-import stay from "../../database/Manager/MusicManager";
 import chalk from "chalk";
 
 @ApplyOptions<ListenerOptions>({
@@ -15,17 +14,18 @@ import chalk from "chalk";
 export class nodeConnectEvent extends Listener {
     async run(node: Node) {
         for (const players of [...this.container.client.manager.players.filter(x => x.node === node).values()]) {
-            const newNode = this.container.client.manager.nodes.get(this.container.client.manager.leastLoadNodes.first()?.options.identifier as string);
+            const newNode = this.container.client.manager.nodes.get(this.container.client.manager.leastLoadNodes.first().options.identifier as string);
 
             const playOptions = {
                 op: "play",
                 guildId: players.guild,
-                track: players.queue.current?.track,
+                track: players.queue.current.track,
                 startTime: players.position,
                 volume: players.volume,
             };
-            await newNode?.send(players.voiceState);
-            await newNode?.send(playOptions);
+
+            await newNode.send(players.voiceState);
+            await newNode.send(playOptions);
             players.node = newNode;
             console.log(chalk.yellow(`🏹 ${players.guild}'s player moved to ${newNode.options.identifier}`))
         }
