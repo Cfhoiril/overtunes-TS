c02c0267c55826b4bebce533e39987bff33ae1ee
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-10-28 13:01:05 +0700

    fix(listCommand): fix map not working

--- src/commands/playlist/ListCommand.ts
+++ src/commands/playlist/ListCommand.ts
@@ -11,22 +11,24 @@ import playlist from "../../database/Manager/PlaylistManager";
 
 export class PlaylistCommand extends Command {
     async messageRun(msg: Message, args: Args) {
-        const data = await playlist.findOne({ User: msg.author.id });
-        if (!data.length) {
-            msg.channel.send({
-                embeds: [new MessageEmbed()
-                    .setDescription('You dont have any saved playlist')
-                    .setColor('RED')
-                ]
-            })
-        } else {
-            let j = 0
-            const pl = new MessageEmbed()
-                .setTitle(`${msg.author.username}'s Playlist`)
-                .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
-                .setColor(msg.guild?.me?.displayHexColor!)
-                .setDescription(`\`\`\`${data.map((c: any) => `❯ ${++j}. ${c.Playlist}`).join("\n")}\`\`\``)
-            msg.channel.send({ embeds: [pl] })
-        }
+        playlist.find({ User: msg.author.id }).then((data) => {
+            if (!data.length) {
+                msg.channel.send({
+                    embeds: [new MessageEmbed()
+                        .setDescription('You dont have any saved playlist')
+                        .setColor('RED')
+                    ]
+                })
+            } else {
+                let j = 0
+                const pl = new MessageEmbed()
+                    .setTitle(`${msg.author.username}'s Playlist`)
+                    .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
+                    .setColor(msg.guild?.me?.displayHexColor!)
+                    .setDescription(`\`\`\`${data.map((c) => `❯ ${++j}. ${c.Playlist}`).join("\n")}\`\`\``)
+                msg.channel.send({ embeds: [pl] })
+            }
+
+        })
     }
 }
