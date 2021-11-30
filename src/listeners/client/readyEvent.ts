bf802e2a4900622ebf0276a46b2f7e11c7c1f4aa
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-12-01 06:06:02 +0700

    feat(readyEvent): remove node dc handler

--- src/listeners/client/readyEvent.ts
+++ src/listeners/client/readyEvent.ts
@@ -49,31 +49,6 @@ export class readyEvent extends Listener {
         this.container.client.manager.init(this.container.client.user!.id);
         this.container.logger.info(chalk.green(`👋 Logged in as ${chalk.white(this.container.client.user?.username)}`));
 
-        // Node disconnect handle to move player
-        this.container.client.manager.on("nodeDisconnect", async (node: Node) => {
-            for (const players of [...this.container.client.manager.players.filter(x => x.node === node).values()]) {
-                if (players.state == "DESTROYING") return players.destroy();
-                const channel = this.container.client.channels?.cache?.get(players?.textChannel!) as TextBasedChannelTypes
-                if (players.get("Message")) channel?.messages?.fetch(players.get("Message")).then((x: any) => x.delete()).catch((e: any) => { });
-
-                const newNode = this.container.client.manager.nodes.get(this.container.client?.manager?.leastLoadNodes.first()?.options.identifier as string);
-
-                const playOptions = {
-                    op: "play",
-                    guildId: players.guild,
-                    track: players.queue.current?.track,
-                    startTime: players.position,
-                    volume: players.volume,
-                };
-
-                await newNode?.send(players.voiceState);
-                await newNode?.send(playOptions);
-                // @ts-ignore
-                players.node = newNode;
-                console.log(chalk.yellow(`🏹 ${this.container.client.guilds?.cache?.get(players.guild)?.name}'s player moved to ${newNode?.options.identifier}`))
-            }
-        })
-
         // Player move handler, since event itself not working
         this.container.client.manager.on("playerMove", async (player: Player, oldChannel: string, newChannel: string) => {
             if (!newChannel) {
