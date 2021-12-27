22a3c6842e1719f89e9225599917f165a4e1f168
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-12-28 00:07:56 +0700

    nodes event

--- src/listeners/nodes/nodeConnectEvent.ts
+++ src/listeners/nodes/nodeConnectEvent.ts
@@ -1,18 +1,16 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Listener, ListenerOptions } from "@sapphire/framework";
-import { Track, Player, Node } from "erela.js";
 import { Client } from "discord.js";
-import stay from "../../database/Manager/MusicManager";
 import chalk from "chalk";
 
 @ApplyOptions<ListenerOptions>({
     name: "nodeConnect",
-    emitter: "manager" as keyof Client,
-    event: "nodeConnect"
+    emitter: "audioManager" as keyof Client,
+    event: "ready"
 })
 
 export class nodeConnectEvent extends Listener {
-    async run(node: Node) {
-        console.log(chalk.green(`🎧 ${node.options.identifier} Connected`))
+    async run(name: string, resumed: boolean) {
+        console.log(chalk.green(`🎧 ${name} Connected ${chalk.white(`Connection was ${chalk.green(resumed ? 'Resumed' : 'New')}`)}`))
     }
 }
--- src/listeners/nodes/nodeDisconnectEvent.ts
+++ src/listeners/nodes/nodeDisconnectEvent.ts
@@ -1,40 +1,17 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Listener, ListenerOptions } from "@sapphire/framework";
-import { Track, Player, Node } from "erela.js";
 import { Client } from "discord.js";
 import chalk from "chalk";
-import { TextBasedChannelTypes } from "@sapphire/discord.js-utilities";
+import { ShoukakuPlayer } from "shoukaku";
 
 @ApplyOptions<ListenerOptions>({
     name: "nodeDisconnect",
-    emitter: "manager" as keyof Client,
-    event: "nodeDisconnect"
+    emitter: "audioManager" as keyof Client,
+    event: "disconnect"
 })
 
 export class nodeConnectEvent extends Listener {
-    async run(node: Node) {
-        for (const players of [...this.container.client.manager.players.filter(x => x.node === node).values()]) {
-            if (players.state == "DESTROYING") return players.destroy();
-            const channel = this.container.client.channels?.cache?.get(players?.textChannel!) as TextBasedChannelTypes
-            // @ts-expect-error
-            if (players.get("Message")) players.get("Message").delete().catch(() => { });
-
-            const newNode = this.container.client.manager.nodes.get(this.container.client?.manager?.leastLoadNodes.first()?.options.identifier as string);
-            if (!newNode) return players.destroy();
-
-            const playOptions = {
-                op: "play",
-                guildId: players.guild,
-                track: players.queue.current?.track,
-                startTime: players.position,
-                volume: players.volume,
-            };
-
-            await newNode?.send(players.voiceState);
-            await newNode?.send(playOptions);
-            // @ts-ignore
-            players.node = newNode;
-            console.log(chalk.yellow(`🏹 ${this.container.client.guilds?.cache?.get(players.guild)?.name}'s player moved to ${newNode?.options.identifier}`))
-        }
+    async run(name: string, players: Array<ShoukakuPlayer>, moved: boolean) {
+        console.log(chalk.yellow(`🏹 ${name} Disconnected, all players are ${moved ? 'Moved' : 'Destroyed'}`))
     }
 }
